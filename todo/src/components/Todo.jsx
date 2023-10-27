da1a33dd746b5fd825c39e390d4c298f630c5d11
Justin Howard <47066746+AldousHuxy@users.noreply.github.com>
2023-10-27 15:37:06 -0600

    init commit

---
+++ todo/src/components/Todo.jsx
@@ -0,0 +1,14 @@
+export const Todo = ({ todo, toggleTodo }) => {
+    const handleTodoClick = () => {
+        toggleTodo(todo.id)
+    }
+
+    return (
+        <div>
+            <label>
+                <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
+                {todo.name}
+            </label>
+        </div>
+    )
+}
